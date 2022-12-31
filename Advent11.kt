23d95016166befb5340cd2ef0a09b221b4285e9d
daro05 <dag.ronnell@svt.se>
2022-12-31 15:20:45 +0100

    Dag 11, del 1

--- Advent11.kt
+++ Advent11.kt
@@ -1,299 +1,170 @@
-import kotlin.math.abs
-
 fun main() {
-    val lines = input11.lines().filter { it.isNotEmpty() }
+    val monkeys = input11.split("Monkey").drop(1).map {
+        Monkey.createFrom(it)
+    }
 
-    println(sum)
+    repeat(20) {
+        monkeys.forEach { monkey ->
+            monkey.items.forEach { item ->
+                monkey.inspections++
+                val operand = if (monkey.operation.operand == "old") {
+                    item
+                } else {
+                    monkey.operation.operand.toInt()
+                }
+
+                val worryLevel = monkey.operation.op.invoke(operand, item).floorDiv(3)
+                val receiver = monkey.getThrowToMonkey(worryLevel)
+                monkeys.find { it.number == receiver }?.items?.add(worryLevel)
+            }
+
+            monkey.items.clear()
+        }
+    }
+
+    val value = monkeys.map { it.inspections }.sorted().reversed().take(2)
+        .fold(1) { acc, i -> acc * i }
+
+    println(value)
 }
 
+data class Monkey(
+    val number: Int,
+    val items: MutableList<Int>,
+    val operation: Operation,
+    val testDivisibleNumber: Int,
+    var receiverIfTrue: Int,
+    var receiverIfFalse: Int,
+    var inspections: Int = 0
+) {
+    fun getThrowToMonkey(worryLevel: Int): Int =
+        if (worryLevel % testDivisibleNumber == 0) {
+            receiverIfTrue
+        } else {
+            receiverIfFalse
+        }
+
+    companion object {
+        fun createFrom(s: String): Monkey {
+            val lines = s.lines()
+            var lineCnt = 0
+            val number = lines[lineCnt++].substringBefore(":").trim().toInt()
+            val items = lines[lineCnt++].substringAfter("Starting items:").split(",").toList()
+                .map { it.trim().toInt() }
+                .toMutableList()
+            val op = Operation.from(lines[lineCnt++].substringAfter("Operation: new = old "))
+            val divisibleBy = lines[lineCnt++].substringAfter("divisible by ").toInt()
+
+            return Monkey(
+                number = number,
+                items = items,
+                operation = op,
+                testDivisibleNumber = divisibleBy,
+                receiverIfTrue = lines[lineCnt++].substringAfter("If true: throw to monkey ").toInt(),
+                receiverIfFalse = lines[lineCnt++].substringAfter("If false: throw to monkey ").toInt()
+            )
+        }
+    }
+}
+
+data class Operation(val operand: String, val op: (Int, Int) -> (Int)) {
+    companion object {
+        fun from(s: String): Operation {
+            val parts = s.trim().split(" ")
+            val op = when (parts[0]) {
+                "+" -> { i1: Int, i2: Int -> i1 + i2 }
+                "*" -> { i1: Int, i2: Int -> i1 * i2 }
+                else -> throw RuntimeException("Can't handle ${parts[0]}")
+            }
+            return Operation(op = op, operand = parts[1])
+        }
+    }
+}
 
 const val input11_simple = """
-addx 15
-addx -11
-addx 6
-addx -3
-addx 5
-addx -1
-addx -8
-addx 13
-addx 4
-noop
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx 5
-addx -1
-addx -35
-addx 1
-addx 24
-addx -19
-addx 1
-addx 16
-addx -11
-noop
-noop
-addx 21
-addx -15
-noop
-noop
-addx -3
-addx 9
-addx 1
-addx -3
-addx 8
-addx 1
-addx 5
-noop
-noop
-noop
-noop
-noop
-addx -36
-noop
-addx 1
-addx 7
-noop
-noop
-noop
-addx 2
-addx 6
-noop
-noop
-noop
-noop
-noop
-addx 1
-noop
-noop
-addx 7
-addx 1
-noop
-addx -13
-addx 13
-addx 7
-noop
-addx 1
-addx -33
-noop
-noop
-noop
-addx 2
-noop
-noop
-noop
-addx 8
-noop
-addx -1
-addx 2
-addx 1
-noop
-addx 17
-addx -9
-addx 1
-addx 1
-addx -3
-addx 11
-noop
-noop
-addx 1
-noop
-addx 1
-noop
-noop
-addx -13
-addx -19
-addx 1
-addx 3
-addx 26
-addx -30
-addx 12
-addx -1
-addx 3
-addx 1
-noop
-noop
-noop
-addx -9
-addx 18
-addx 1
-addx 2
-noop
-noop
-addx 9
-noop
-noop
-noop
-addx -1
-addx 2
-addx -37
-addx 1
-addx 3
-noop
-addx 15
-addx -21
-addx 22
-addx -6
-addx 1
-noop
-addx 2
-addx 1
-noop
-addx -10
-noop
-noop
-addx 20
-addx 1
-addx 2
-addx 2
-addx -6
-addx -11
-noop
-noop
-noop
+Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1
 """
 
 const val input11 = """
-noop
-noop
-noop
-addx 5
-noop
-addx 1
-addx 2
-addx 5
-addx 2
-addx 1
-noop
-addx 5
-noop
-addx -1
-noop
-addx 5
-noop
-noop
-addx 5
-addx 1
-noop
-noop
-addx 3
-addx 2
-noop
-addx -38
-noop
-addx 3
-addx 2
-addx -5
-addx 12
-addx 2
-addx 27
-addx -40
-addx 19
-addx 2
-addx 19
-addx -18
-addx 2
-addx 5
-addx 2
-addx -23
-addx 22
-addx 4
-addx -34
-addx -1
-addx 5
-noop
-addx 2
-addx 1
-addx 20
-addx -17
-noop
-addx 25
-addx -17
-addx -2
-noop
-addx 3
-addx 19
-addx -12
-addx 3
-addx -2
-addx 3
-addx 1
-noop
-addx 5
-noop
-noop
-addx -37
-addx 3
-addx 4
-noop
-addx 24
-addx -6
-addx -15
-addx 2
-noop
-addx 6
-addx -2
-addx 6
-addx -12
-addx -2
-addx 19
-noop
-noop
-noop
-addx 3
-noop
-addx 7
-addx -2
-addx -24
-addx -11
-addx 4
-addx 3
-addx -2
-noop
-addx 7
-addx -2
-addx 2
-noop
-addx 3
-addx 7
-noop
-addx -2
-addx 5
-addx 2
-addx 5
-noop
-noop
-noop
-addx 3
-addx -35
-addx 35
-addx -21
-addx -14
-noop
-addx 5
-addx 2
-addx 33
-addx -7
-addx -23
-addx 5
-addx 2
-addx 1
-noop
-noop
-addx 5
-addx -1
-noop
-addx 3
-addx -23
-addx 30
-addx 1
-noop
-addx 4
-addx -17
-addx 11
-noop
-noop
+Monkey 0:
+  Starting items: 54, 61, 97, 63, 74
+  Operation: new = old * 7
+  Test: divisible by 17
+    If true: throw to monkey 5
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 61, 70, 97, 64, 99, 83, 52, 87
+  Operation: new = old + 8
+  Test: divisible by 2
+    If true: throw to monkey 7
+    If false: throw to monkey 6
+
+Monkey 2:
+  Starting items: 60, 67, 80, 65
+  Operation: new = old * 13
+  Test: divisible by 5
+    If true: throw to monkey 1
+    If false: throw to monkey 6
+
+Monkey 3:
+  Starting items: 61, 70, 76, 69, 82, 56
+  Operation: new = old + 7
+  Test: divisible by 3
+    If true: throw to monkey 5
+    If false: throw to monkey 2
+
+Monkey 4:
+  Starting items: 79, 98
+  Operation: new = old + 2
+  Test: divisible by 7
+    If true: throw to monkey 0
+    If false: throw to monkey 3
+
+Monkey 5:
+  Starting items: 72, 79, 55
+  Operation: new = old + 1
+  Test: divisible by 13
+    If true: throw to monkey 2
+    If false: throw to monkey 1
+
+Monkey 6:
+  Starting items: 63
+  Operation: new = old + 4
+  Test: divisible by 19
+    If true: throw to monkey 7
+    If false: throw to monkey 4
+
+Monkey 7:
+  Starting items: 72, 51, 93, 63, 80, 86, 81
+  Operation: new = old * old
+  Test: divisible by 11
+    If true: throw to monkey 0
+    If false: throw to monkey 4
 """
